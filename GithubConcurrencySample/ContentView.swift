cb0dcaee20a7143de6442f8d79a86d28cdd42759
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-09-16 14:31:44 +0900

    Initial Commit

---
+++ GithubConcurrencySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubConcurrencySample
+//
+//  Created by Yusuke Hasegawa on 2021/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubConcurrencySample/GithubConcurrencySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubConcurrencySampleApp.swift
+//  GithubConcurrencySample
+//
+//  Created by Yusuke Hasegawa on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct GithubConcurrencySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
